5983bb51091c8b1a8b25571c96b8084f827af9b1
Suraj <surajyadav052005@gmail.com>
2024-09-17 23:49:10 +0530

    start of my logic bulding

---
+++ bubbleSort2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[10] = {1, 2, 7 , 8, 9, 4, 6, 2, 6, 2};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int start = 0;
+    int end = n-1;
+
+    while(start<=end){
+        
+    }
+}
