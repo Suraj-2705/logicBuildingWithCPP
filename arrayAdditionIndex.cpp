06b7d61f11050ba52503ba1f9211458a31a8a917
Suraj <surajyadav052005@gmail.com>
2024-09-24 12:52:12 +0530

    cummulative array

---
+++ arrayAdditionIndex.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+void addArray(int arr[], int x[] , int n)
+{
+  x[0] = arr[0];
+
+  for(int i = 0; i < n; i++){
+    x[i+1] = arr[i+1] + x[i];
+  }
+}
+int main()
+{
+    int arr[] = {2, -5, 3, 2, 4, 6};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int x[n];
+    addArray(arr, x, n);
+    for(int i = 0; i<n; i++){
+        cout<<x[i]<<" ";
+    }
+}
