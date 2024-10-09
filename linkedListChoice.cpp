5e619d7f98ae45cd042ca465872f9b0c42a0a3c0
Suraj <surajyadav052005@gmail.com>
2024-10-09 12:43:22 +0530

    linked list menu

--- linkedListChoice.cpp
+++ linkedListChoice.cpp
@@ -13,6 +13,7 @@ class Node{
 
     
 };
+
 void printList(Node * head)
     {
         if(head == NULL)
@@ -46,6 +47,15 @@ void insertAtBeg(Node **head, int value){
     nodeObj->next = *head;
     *head = nodeObj;
 }
+
+void deleteFromBeginning(Node **head){
+    if(*head == NULL){
+        cout << "No element to be deleted";
+    }else{
+        Node *temp = *head;
+        *head = temp->next;
+    }
+}
 int main(){
     Node * head = NULL;
     int choice, value, n;
@@ -55,7 +65,8 @@ int main(){
         cout << "1. Insert element at the beginning of linked list\n";
         cout << "2. Insert element at the end of linked list\n";
         cout << "3. Display linked list\n";
-        cout << "4. Exit\n";
+        cout << "4. Delete the element from the beginning\n";
+        cout << "5. Exit\n";
         cout << "Enter your choice: ";
         cin >> choice;
 
@@ -82,13 +93,23 @@ int main(){
                 printList(head);
                 break;
             case 4:
+                cout << "Enter the number of elements to delete: ";
+                cin >> n;
+                cout << "Enter the value to be deleted: ";
+                for(int i = 0; i < n; i++){
+                cin >> value;
+                deleteFromBeginning(&head);
+                }
+                break;
+                
+            case 5:
                 cout << "Exiting program.\n";
                 return 0;
             default:
                 cout << "Invalid choice. Please try again.\n";
         }
     }
-    
+
     return 0;
 
 }
