f404cdb8a2264ddef1ea07aa4a2f4a1e4112c37a
Suraj <surajyadav052005@gmail.com>
2024-11-06 23:54:43 +0530

    extended euclidian algorithm

---
+++ extentedEuclidianAlgorithm.cpp
@@ -0,0 +1,34 @@
+
+//Introduction and use of Euclidean algorithm
+int gcd(int a, int b)
+{
+    if (a == 0)
+        return b;
+    return gcd(b % a, a);
+}
+
+//Extended Euclidean algorithm and its recursive implementation
+class Solution{
+public:
+    int findGcd(int divisor,int dividend,int *x,int *y)
+    {
+        if(divisor==0)
+        {
+            *x=0;
+            *y=1;
+            return dividend;
+        }
+        int x1,y1;
+        int ans=findGcd(dividend%divisor, divisor, &x1, &y1);
+        *x=y1-(dividend/divisor)*x1;
+	*y=x1;
+        return ans;
+    }
+    vector<int> gcd(int divisor, int dividend){
+        // code here
+        int x,y;
+        int gcd= findGcd(divisor, dividend, &x, &y);
+        return {gcd,x,y};
+        
+    }
+};
